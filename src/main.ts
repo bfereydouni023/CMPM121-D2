a2a53ced07cdc6875325f5f5547fcf4fa06a5ca2
bfereydouni023 <bfereydo@gmail.com>
2025-12-09 19:21:28 -0800

    deleted unneeded starter code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
-
-//test
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
